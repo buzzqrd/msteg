6a24ae715cee240c0447913c40d9477be9110824
buzzqrd <buzzqrd@protonmail.com>
2023-02-23 22:40:35 -0800

    split to multiple files

---
+++ defs.h
@@ -0,0 +1,22 @@
+/*
+defs.h - global definitions for msteg
+buzzqrd
+*/
+
+#ifndef _DEFS_H
+#define _DEFS_H
+
+
+#define TRUE 1
+#define FALSE 0
+
+#define CHUNK_SIZE 32
+#define FIELD_SIZE 4
+
+#define STR_TAG 0x01
+#define BIN_TAG 0x02
+
+#define FILE_WRITE_CHUNK_SIZE 512
+
+
+#endif
